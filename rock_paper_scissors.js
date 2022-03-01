cbb053838621cd749c70ccdc8395b8895aca9222
dfe325 <daneder@gmail.com>
2022-03-01 15:01:33 -0500

    Clean up V1

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,30 +1,40 @@
+// rock_paper_scissors.js
+// this program plays 5 rounds of rock paper rock_paper_scissors
+// the program gets player input from a prompt
+// then randomly selects r, p, or s for the computer
+// then tabulates whether this was a win, lose, or draw for the player
+// at the end of 5 rounds, it reports whether the winner = player, CPU, or tie
+
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.toLowerCase();
 
   if (playerSelection == computerSelection) {
-    return "Tie! Try again!"
+    return "Tie! Try again!";
 
   }
 
-  if (playerSelection === "rock" && computerSelection != "paper" || playerSelection === "paper" && computerSelection != "scissors" || playerSelection === "scissors" && computerSelection != "rock") {
-    return `You win! ${playerSelection} beats ${computerSelection}`
+  if (playerSelection === "rock" && computerSelection != "paper" ||
+      playerSelection === "paper" && computerSelection != "scissors" ||
+          playerSelection === "scissors" && computerSelection != "rock") {
+    return `You win! ${playerSelection} beats ${computerSelection}`;
   } else {
-    return `You lose! ${computerSelection} beats ${playerSelection}`
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
   }
 }
 
-function game () {
+function playGame () {
   let player = 0;
   let computer = 0;
   let tie = 0;
-  const outcome_list = ['Player', 'Computer', 'Tie'];
+  const outcomeList = ['Player', 'Computer', 'Tie'];
 
   for (let roundsPlayed = 0; roundsPlayed < 5; roundsPlayed++) {
-    let computerPlay = ['scissors', 'paper', 'rock'];
-    let computerSelection = computerPlay[Math.floor(Math.random() * computerPlay.length)]
+    let computerOptions = ['scissors', 'paper', 'rock'];
+    let computerSelection = computerOptions[Math.floor(Math.random() *
+        computerOptions.length)]
     const playerSelection = window.prompt("Rock, paper, or scissors?");
     let outcome = playRound(playerSelection, computerSelection);
-    
+
     if (outcome.includes("You win!")) {
       player += 1;
     } else if (outcome.includes("You lose!")) {
@@ -37,14 +47,14 @@ function game () {
     outcome = playRound(playerSelection, computerSelection);
 
     }
-    let final_tally = [player, computer, tie];
-    let winner = Math.max.apply(Math, final_tally);
+    let finalTally = [player, computer, tie];
+    let winner = Math.max.apply(Math, finalTally);
 
-    if (final_tally[0] == final_tally[1]) {
-      return "Tie."
+    if (finalTally[0] == finalTally[1]) {
+      return "Tie.";
     } else {
-    return `The winner is ${outcome_list[final_tally.indexOf(winner)]} with ${winner}`;
+    return `${outcomeList[finalTally.indexOf(winner)]} wins with ${winner}`;
     }
 }
 
-console.log(game());
+console.log(playGame());
